42b49dc163f856233cea229c604a034f63241267
vincentliu98 <jie.liu0731@gmail.com>
2018-11-09 13:43:31 -0500

    finished sideview's treeView

---
+++ src/frontend/src/authoringInterface/sidebar/EntitySubTreeView.java
@@ -0,0 +1,15 @@
+package authoringInterface.sidebar;
+
+import javafx.scene.control.TreeItem;
+
+public class EntitySubTreeView extends SubTreeView {
+
+    public EntitySubTreeView() {
+        super("Entity");
+        for (int i = 1; i < 3; i++) {
+            TreeItem<String> item = new TreeItem<>("Entity" + i);
+            getRootItem().getChildren().add(item);
+        }
+    }
+
+}
---
+++ src/frontend/src/authoringInterface/sidebar/TilesetsSubTreeView.java
@@ -0,0 +1,14 @@
+package authoringInterface.sidebar;
+
+import javafx.scene.control.TreeItem;
+
+public class TilesetsSubTreeView extends SubTreeView {
+
+    public TilesetsSubTreeView() {
+        super("Tile Sets");
+        for (int i = 1; i < 3; i++) {
+            TreeItem<String> item = new TreeItem<>("Tile Sets" + i);
+            getRootItem().getChildren().add(item);
+        }
+    }
+}
