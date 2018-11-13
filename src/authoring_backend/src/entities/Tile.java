436106743667ef89787d1fe270319f7702ae54b4
Jason Zhou <jz192@duke.edu>
2018-11-13 00:45:48 -0500

    test

--- src/authoring_backend/src/entities/Tile.java
+++ src/authoring_backend/src/entities/Tile.java
@@ -1,8 +1,5 @@
-<<<<<<< HEAD:src/authoring_backend/entities/Tile.java
-package authoring_backend.entities;
-=======
 package entities;
->>>>>>> master:src/authoring_backend/src/entities/Tile.java
+
 
 import java.util.Iterator;
 import java.util.Set;
