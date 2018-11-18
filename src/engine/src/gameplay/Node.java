72fb877459caf408ac5d453ab154f6bd0895da57
Natalie Le <nl121@duke.edu>
2018-11-18 17:40:59 -0500

    Debugging script engine

--- src/engine/src/gameplay/Node.java
+++ src/engine/src/gameplay/Node.java
@@ -33,12 +33,15 @@ public class Node {
         try{
             ScriptEngine engine = new ScriptEngineManager().getEngineByName("groovy");
             System.out.println("reaching here 2nd");
-            engine.put("GameData", new GameData());
+            //GameData gd = new GameData();
+            engine.put("gd", 1);
+            //engine.put("GameData", new GameData());
             System.out.println("reaching here 3rd");
-            engine.eval(myExecution);
+            //engine.eval(myExecution);
             System.out.println("after execution");
-        } catch (ScriptException e){
+        } catch (Exception e){
             System.out.println("Script exception in Edge");
+            e.printStackTrace();
         }
         //GameData.clearArguments();
         // execute Groovy execution that defines which edge to execute (which ID)
--- src/engine/src/module-info.java
+++ src/engine/src/module-info.java
@@ -7,10 +7,9 @@ module engine {
     requires java.xml;
     requires java.scripting;
     requires xstream;
-    //requires groovy.all;
+    requires org.codehaus.groovy;
 
-    opens gameplay to xstream;
-    //opens gameplay to groovy.all;
+    opens gameplay to xstream, org.codehaus.groovy;
     exports gameplay;
 
 }
