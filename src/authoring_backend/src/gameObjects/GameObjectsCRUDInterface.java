cd186d8497058560c139f9308f6be96dfdf08a6f
Jason Zhou <jz192@duke.edu>
2018-11-27 12:30:14 -0500

    added some documentation for gameObjects

--- src/authoring_backend/src/gameObjects/GameObjectsCRUDInterface.java
+++ src/authoring_backend/src/gameObjects/GameObjectsCRUDInterface.java
@@ -2,12 +2,14 @@ package gameObjects;
 
 /**
  *
- * This class encapsulates the Game Data for the Entities (Tiles and Sprites). It provides methods to create, edit, and delete the Sprites and Tiles Classes.
+ * This class encapsulates the Game Data for the GameObjects (Tiles and Entities). It provides methods to create, edit, and delete the Tile and Entity Classes.
+ * It holds maps of GameObject Classes as well as GameObject Instances.
  * @author Jason Zhou
  */
 public interface GameObjectsCRUDInterface {
 
     /**
+     * This method creates a Tile Class and adds it to the map.
      * @param name the name of the Tile Class to be created
      * @return the Tile Class if there is no name collision and the method is successful
      * @throws DuplicateClassException if a class with the same name exists
@@ -15,6 +17,7 @@ public interface GameObjectsCRUDInterface {
     TileClass createTileClass(String name);
 
     /**
+     * This method gets the Tile Class from the map.
      * @param name the name of the Tile Class to be retrieved
      * @return the Tile Class with the name
      * @throws NoTileClassException if there is no such Tile Class
@@ -22,20 +25,21 @@ public interface GameObjectsCRUDInterface {
     TileClass getTileClass(String name);
 
     /**
-     *
+     * This method deletes the specified Tile Class.
      * @param name the name of the Tile Class to be deleted
      * @return True if the Tile Class exists
      */
     boolean deleteTileClass(String name);
 
     /**
-     *
-     * @param instanceId
-     * @return
+     * This method deletes the specified Tile Instance.
+     * @param instanceId the id of the instance
+     * @return True if the Tile Instance with the instanceId exists
      */
     boolean deleteTileInstance(int instanceId);
 
     /**
+     * 
      * @param name the name of the Entity Class to be created
      * @return the Entity Class if there is no name collision and the method is successful
      * @throws DuplicateClassException if a class with the same name exists
