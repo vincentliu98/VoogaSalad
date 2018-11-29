caa9e57840ef3080db2877218a4fc07f84ca9b46
vincentliu98 <jie.liu0731@gmail.com>
2018-11-28 21:55:50 -0500

    added basic framework for TileEditor

--- src/frontend/src/authoringInterface/subEditors/TileEditor.java
+++ src/frontend/src/authoringInterface/subEditors/TileEditor.java
@@ -1,12 +1,59 @@
 package authoringInterface.subEditors;
 
 import gameObjects.GameObjectsCRUDInterface;
+import gameObjects.entity.EntityClass;
 import gameObjects.tile.TileClass;
 import gameObjects.tile.TileInstance;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeItem;
+import javafx.stage.Stage;
+
+/**
+ * Editor to change the Tile settings. Need to work on it. Low priority
+ *
+ * @author jl729
+ */
 
 public class TileEditor extends AbstractGameObjectEditor<TileClass, TileInstance> {
+    private TextField widthText = new TextField();
+    private TextField heightText = new TextField();
+    private TextField nameText = new TextField();
+
     public TileEditor(GameObjectsCRUDInterface manager) {
         super(manager);
+        widthText.setPromptText("Width");
+        heightText.setPromptText("Height");
+        nameText.setPromptText("Tile Name");
+        confirm.setOnAction(e -> {
+            if (nameField.getText().trim().isEmpty()) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Empty Name");
+                alert.setContentText("You must give your entity a non-empty name");
+                alert.showAndWait();
+            } else {
+                ((Stage) rootPane.getScene().getWindow()).close();
+                switch (editingMode) {
+                    case ADD_TREEITEM:
+                        TreeItem<String> newItem = new TreeItem<>();
+//                        entityClass.addImagePath(imagePath);
+//                        ImageView icon = new ImageView(preview.getImage());
+//                        icon.setFitWidth(50);
+//                        icon.setFitHeight(50);
+//                        newItem.setGraphic(icon);
+                        treeItem.getChildren().add(newItem);
+                        break;
+                    case NONE:
+                        return;
+                    case EDIT_NODE:
+                        break;
+                    case EDIT_TREEITEM:
+                        break;
+                }
+            }
+        });
+        setupLayout();
+        rootPane.getChildren().addAll();
     }
 
     /**
@@ -22,6 +69,10 @@ public class TileEditor extends AbstractGameObjectEditor<TileClass, TileInstance
      */
     @Override
     public void readGameObjectClass() {
+
+    }
+
+    private void setupLayout() {
 
     }
 }
