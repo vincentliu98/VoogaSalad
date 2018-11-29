b2a94558b98ab2fc79abbf51e48bd01d462beeaf
Jonathan Nakagawa <jyn2@duke.edu>
2018-11-29 04:31:48 -0500

    configured to work with relative paths

--- src/controller/src/launchingGame/LauncherGamesDisplay.java
+++ src/controller/src/launchingGame/LauncherGamesDisplay.java
@@ -6,12 +6,14 @@ import launching.GameParser;
 import java.util.List;
 
 public class LauncherGamesDisplay {
-    public static String CSS_PATH = "launcher-games-display";
-    public static int COLUMN_NUMBER = 4;
+    public static final String CSS_PATH = "launcher-games-display";
+    public static final int COLUMN_NUMBER = 4;
+    public static final String CURRENT_FOLDER_KEY = "user.dir";
+    public static final String GAMES_PATH = "/src/controller/resources/games/";
 
 
-    public static double HOR_SPACING = 29;
-    public static double VER_SPACING = 20;
+    public static final double HOR_SPACING = 29;
+    public static final double VER_SPACING = 20;
 
     private TilePane myPane;
     private List<GameIcon> myGames;
@@ -31,17 +33,12 @@ public class LauncherGamesDisplay {
     }
 
     private void initGames(){
-        GameParser myParser = new GameParser("/Users/jonathannakagawa/Desktop/Stuff/CompSci308/voogasalad_printstacktrace/src/controller/resources/games");
+        //GameParser myParser = new GameParser("/Users/jonathannakagawa/Desktop/Stuff/CompSci308/voogasalad_printstacktrace/src/controller/resources/games");
+        GameParser myParser = new GameParser(System.getProperty(CURRENT_FOLDER_KEY) + GAMES_PATH);
         myGames = myParser.getMyGames();
         for(GameIcon myIcon: myGames){
             myPane.getChildren().add(myIcon.getView());
         }
-//        for(int i = 0; i < 6; i++){
-////            GameIcon myicon = new GameIcon("Chess", "blah");
-////            myGames.add(myicon);
-////            myPane.getChildren().add(myicon.getView());
-//        }
-
 
     }
 
