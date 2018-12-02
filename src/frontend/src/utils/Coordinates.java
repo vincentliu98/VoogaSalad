b8695aca890f22e93f3619985ca8e22c0c9a2670
Haotian Wang <haotian.wang@duke.edu>
2018-12-02 16:30:22 -0500

    Finished writing the one and only Coordinates class

--- src/frontend/src/utils/Coordinates.java
+++ src/frontend/src/utils/Coordinates.java
@@ -1,6 +1,8 @@
 package utils;
 
 import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 
 /**
  * This class provides convenient methods to set the coordinates of some JavaFx Nodes.
@@ -8,7 +10,23 @@ import javafx.scene.Node;
  * @author Haotian Wang
  */
 public class Coordinates {
-    void setXAndY(Node node, double x, double y) {
-        if (node instanceof )
+    /**
+     * This method sets the coordinates for an unknown type of JavaFx Node.
+     *
+     * @param node: A Node whose coordinates will be set.
+     * @param x: A double value for the x coordinate.
+     * @param y: A double value for the y coordinate.
+     * @throws UnhandledCoordinatesClassException
+     */
+    public static void setXAndY(Node node, double x, double y) throws UnhandledCoordinatesClassException {
+        if (node instanceof ImageView) {
+            ((ImageView) node).setX(x);
+            ((ImageView) node).setY(y);
+        } else if (node instanceof Text) {
+            ((Text) node).setX(x);
+            ((Text) node).setY(y);
+        } else {
+            throw new UnhandledCoordinatesClassException("The setXAndY method is not defined for this particular JavaFx Node class.");
+        }
     }
 }
