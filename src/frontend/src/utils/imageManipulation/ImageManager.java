aca0da419fb41f4ca4253dadfc40fe23c9e9faad
Haotian Wang <haotian.wang@duke.edu>
2018-12-02 21:59:17 -0500

    Finished skeleton for ImageManager

---
+++ src/frontend/src/utils/imageManipulation/ImageManager.java
@@ -0,0 +1,34 @@
+package utils.imageManipulation;
+
+import gameObjects.gameObject.GameObjectClass;
+import gameObjects.gameObject.GameObjectInstance;
+import gameObjects.gameObject.GameObjectType;
+import javafx.scene.image.Image;
+
+/**
+ * This ImageManager will provide an Image that is a "preview" of a GameObjectClass. This Image is a JavaFx Image regardless of whether the actual thing being presented is a String.
+ * This class also has functionalities that stack the multiple Images of a GameObjectClass if it has one.
+ *
+ * @author Haotian Wang
+ */
+public class ImageManager {
+    /**
+     * This method gets a preview Image for the input GameObjectClass.
+     *
+     * @param gameObjectClass: The input GameObjectClass.
+     * @return An Image object.
+     */
+    public static Image getPreView(GameObjectClass gameObjectClass) {
+        
+    }
+
+    /**
+     * This method gets an Image that represents a preview of a GameObjectInstance.
+     *
+     * @param gameObjectInstance: A GameObjectInstance whose preview will be returned.
+     * @return An Image object.
+     */
+    public static Image getPreview(GameObjectInstance gameObjectInstance) {
+
+    }
+}
