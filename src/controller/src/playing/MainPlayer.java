414943db7fe6dd47567b840e8280861d305a844c
Natalie Le <nl121@duke.edu>
2018-12-09 18:24:27 -0500

    Saving button for game in Engine

--- src/controller/src/playing/MainPlayer.java
+++ src/controller/src/playing/MainPlayer.java
@@ -1,11 +1,15 @@
 package playing;
 
 import authoringInterface.View;
+import gameplay.GameData;
 import gameplay.Initializer;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.File;
+import java.io.FileWriter;
 
 public class MainPlayer {
 
@@ -25,7 +29,21 @@ public class MainPlayer {
         myInitializer = new Initializer(new File(getClass().getClassLoader().getResource(gameName).getFile()));
         myStage = new Stage();
         myInitializer.setScreenSize(700, 500);
-        Scene newScene = new Scene(myInitializer.getRoot(), View.GAME_WIDTH, View.GAME_HEIGHT);
+        Scene newScene = new Scene(myInitializer.getRoot(), View.GAME_WIDTH, View.GAME_HEIGHT + 50);
+        Button saveButton = new Button("Save state");
+        saveButton.setLayoutY(View.GAME_HEIGHT);
+        saveButton.setMinHeight(50);
+        saveButton.setMinWidth(View.GAME_WIDTH);
+        saveButton.setOnMouseClicked(e -> {
+            String fileContent = GameData.saveGameData();
+            try{
+                FileWriter fileWriter = new FileWriter("c:/temp/samplefile2.txt");
+                fileWriter.write(fileContent);
+                fileWriter.close();
+                GameData.saveGameData(); // TODO: Add to User's 
+            } catch (Exception ex){ }
+        });
+        myInitializer.getRoot().getChildren().add(saveButton);
         myStage.setScene(newScene);
         myStage.show();
     }
