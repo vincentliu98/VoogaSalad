9b0fab1769e94d9b10aac997132af096dd2d726d
Haotian Wang <haotian.wang@duke.edu>
2018-12-09 12:57:21 -0500

    Write my own converter for Authoring

---
+++ src/authoring_backend/src/conversion/authoring/CRUDConverterAuthoring.java
@@ -0,0 +1,11 @@
+package conversion.authoring;
+
+import com.thoughtworks.xstream.converters.Converter;
+
+/**
+ * This Converter is for the authoring GameObjectsCRUD saving and loading.
+ *
+ * @author Haotian Wang
+ */
+public class CRUDConverterAuthoring implements Converter {
+}
